78591da3f5f35ceb458cee8151dc3a00255912ad
Robley Gori <robley@statuscreative.co.ke>
2017-05-08 20:58:20 +0300

    Added empty test cases for non-integer inputs, and type of output

--- Day 1/test_prime_numbers.py
+++ Day 1/test_prime_numbers.py
@@ -4,4 +4,17 @@ from prime_numbers import *
 
 class PrimeNumbersTest(unittest.TestCase):
 	""" Test cases for the function to generate prime numbers """
-	pass
+	def test_if_result_is_a_list(self):
+		""" Check whether the result is a list """
+		result = prime_numbers_generator(12)
+		self.assertTrue(result, list)
+
+	def test_empty_list_if_n_less_than_two(self):
+		""" Check whether an empty list is returned if there are no
+		prime numbers within the range provided """
+		pass
+
+	def test_raises_error_if_n_not_int(self):
+		""" Check whther an error is returned if n is not an integer """
+		pass
+
