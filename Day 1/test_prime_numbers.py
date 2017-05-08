270fb3737245cf73f35a37fcb2ae031fb173d39f
Robley Gori <robley@statuscreative.co.ke>
2017-05-08 21:17:42 +0300

    Added a test for numbers that are not prime numbers and raising errors when input is not an integer

--- Day 1/test_prime_numbers.py
+++ Day 1/test_prime_numbers.py
@@ -1,5 +1,5 @@
 import unittest
-from prime_numbers import *
+from prime_numbers import prime_numbers_generator
 
 
 class PrimeNumbersTest(unittest.TestCase):
@@ -12,13 +12,23 @@ class PrimeNumbersTest(unittest.TestCase):
 	def test_empty_list_if_n_less_than_two(self):
 		""" Check whether an empty list is returned if there are no
 		prime numbers within the range provided """
-		pass
-
-	def test_raises_error_if_n_not_int(self):
-		""" Check whther an error is returned if n is not an integer """
-		pass
+		result = prime_numbers_generator(1)
+		self.assertEqual(result, [])
 
 	def test_if_twenty_three_is_prime(self):
 		""" Check whether 23 in the list of prime numbers generated """
 		result = prime_numbers_generator(24)
 		self.assertIn(23, result)
+
+	def test_no_non_primes_are_returned(self):
+		""" Check that the list that is returned does not include numbers
+		that are not prime numbers"""
+		pass
+
+	def test_raises_error_if_n_is_list(self):
+		""" Check whther an error is raised if n is a list """
+		pass
+
+	def test_raises_error_if_n_is_a_string(self):
+		""" Check whether an error is raised if n is a string """
+		pass
