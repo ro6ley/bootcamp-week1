5a5e0afec23ade8182637cefd61db4a990cb2849
Robley Gori <robley@statuscreative.co.ke>
2017-05-09 09:45:22 +0300

    Added functionality to pass the tests created earlier

--- Day 2/OOP/oop.py
+++ Day 2/OOP/oop.py
@@ -2,13 +2,10 @@ class MobileNewtworks(object):
     """
     Class for the general mobile networks
     """
-    def __init__(self):
+    def buy_airtime(self, amount):
         pass
 
-    def buy_airtime(self):
-        pass
-
-    def send_airtime(self):
+    def send_airtime(self, amount):
         pass
 
 
@@ -16,22 +13,56 @@ class Safaricom(MobileNewtworks):
     """
     Class for the Safaricom object
     """
-    def __init__(self):
-        super(Safaricom, self).__init__()
+    def __init__(self, balance):
+        self.balance = balance
 
-    def buy_airtime(self):
-        pass
+    def buy_airtime(self, amount):
+        if isinstance(amount, int) and amount is not None:
+            if amount >= 0:
+                self.balance += amount
+                return self.balance
+            else:
+                return "Error: Invalid amount"
+        else:
+            return "Only integers allowed"
 
-    def send_airtime(self):
-        pass
+    def send_airtime(self, amount):
+        if isinstance(amount, int) and amount is not None:
+            if amount >= 0:
+                if (self.balance - amount) > 5:
+                    self.balance -= amount
+                    return self.balance
+                else:
+                    return "Insufficient balance"
+            else:
+                return "Invalid amount entered"
+        else:
+            return "Only integers allowed"
 
 class Airtel(MobileNewtworks):
     """Class for the Airtel object"""
-    def __init__(self):
-        super(Airtel, self).__init__()
+    def __init__(self, balance):
+        self.balance = balance
 
-    def buy_airtime(self):
-        pass
+    def buy_airtime(self, amount):
+        if isinstance(amount, int) and amount is not None:
+            if amount >= 0:
+                self.balance += amount
+                return self.balance
+            else:
+                return "Error: Invalid amount"
+        else:
+            return "Only integers allowed"
 
-    def send_airtime(self):
-        pass
+    def send_airtime(self, amount):
+        if isinstance(amount, int) and amount is not None:
+            if amount >= 0:
+                if (self.balance - amount) > 5:
+                    self.balance -= amount
+                    return self.balance
+                else:
+                    return "Insufficient balance"
+            else:
+                return "Invalid amount entered"
+        else:
+            return "Only integers allowed"
