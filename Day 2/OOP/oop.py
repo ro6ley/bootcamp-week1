67013d2729dcdea699621192705798271fac2b55
Robley Gori <robley@statuscreative.co.ke>
2017-05-09 09:27:05 +0300

    Added empty classes for the Mobile Networks objects

---
+++ Day 2/OOP/oop.py
@@ -0,0 +1,37 @@
+class MobileNewtworks(object):
+    """
+    Class for the general mobile networks
+    """
+    def __init__(self):
+        pass
+
+    def buy_airtime(self):
+        pass
+
+    def send_airtime(self):
+        pass
+
+
+class Safaricom(MobileNewtworks):
+    """
+    Class for the Safaricom object
+    """
+    def __init__(self):
+        super(Safaricom, self).__init__()
+
+    def buy_airtime(self):
+        pass
+
+    def send_airtime(self):
+        pass
+
+class Airtel(MobileNewtworks):
+    """Class for the Airtel object"""
+    def __init__(self):
+        super(Airtel, self).__init__()
+
+    def buy_airtime(self):
+        pass
+
+    def send_airtime(self):
+        pass
