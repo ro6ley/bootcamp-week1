3825971fe6b6d70c5a902025bd004aa6f169353d
Robley Gori <robleyadrian@gmail.com>
2017-05-10 12:06:33 +0300

    Instantiated the consumer key, consumer secret and access token variables

---
+++ Day 3/HTTP_Web/twitter_cli.py
@@ -0,0 +1,14 @@
+import tweepy
+import sys
+
+# Twitter consumer key
+consumer_key = ""
+
+# Twitter consumer secret
+consumer_secret = ""
+
+# Twitter access token
+access_token = ""
+
+# Twitter access token secret
+access_token_secret = ""
