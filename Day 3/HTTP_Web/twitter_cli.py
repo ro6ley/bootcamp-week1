ab7b2c25a81080816f3ed43f6cf7e2462391890b
Robley Gori <robleyadrian@gmail.com>
2017-05-10 15:01:28 +0300

    Removed the main function that handles the front end in order to use Docopt instead

--- Day 3/HTTP_Web/twitter_cli.py
+++ Day 3/HTTP_Web/twitter_cli.py
@@ -18,35 +18,6 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-usage = """
-        This is a simple command line interface to the Twitter API using the Tweepy Library.
-
-        Usage:
-
-            python twitter_cli.py [OPTIONS] [USERNAME|QUERY]
-
-            options:
-                --user [username]       :  prints a user details from a given username
-                --search [query]        :  prints search results for a particular query
-                --timeline [username]   :  prints the last tweets of a user
-                --followers [username]  :  prints the usernames of the followers of a particular user
-                --friends [username]    :  prints the usernames of the people a user follows
-                --help                  :  prints this usage message
-
-        Examples:
-            To get NaiRobley's details:
-
-                python twitter_cli.py --user NaiRobley
-
-            To get the usernames of NaiRobley's followers:
-
-                python twitter_cli.py --followers NaiRobley
-
-            To search for users called 'Robley':
-
-                python twitter_cli.py --search Robley
-        """
-
 
 def get_user_details(username):
     """
@@ -57,119 +28,71 @@ def get_user_details(username):
      - Friends
      - Followers count
     """
-    user = api.get_user(username)
-    print('\n{}\'s details:'.format(username))
-    print('\tName: ' + user.name)
-    print('\tUsername: ' + user.screen_name)
-    print('\tLocation: ' + user.location)
-    print('\tFriends: ' + str(user.friends_count))
-    print('\tFollowers Count: ' + str(user.followers_count))
-    print('')
+    try:
+        user = api.get_user(username)
+        print('\n{}\'s details:'.format(username))
+        print('\tName: ' + user.name)
+        print('\tUsername: ' + user.screen_name)
+        print('\tLocation: ' + user.location)
+        print('\tFriends: ' + str(user.friends_count))
+        print('\tFollowers Count: ' + str(user.followers_count))
+        print('')
+    except TweepError:
+        print("Check the username you provided or your connection and try again")
 
 
 def get_user_tweets(username):
     """
     This function prints the latest tweets of a particular user given the username
     """
-    timeline = api.user_timeline(username)
-    print('\n{}\'s latest tweets:'.format(username))
-    for tweet in timeline:
-        print('\t'+tweet.text)
-        print('')
+    try:
+        timeline = api.user_timeline(username)
+        print('\n{}\'s latest tweets:'.format(username))
+        for tweet in timeline:
+            print('\t' + tweet.text)
+            print('')
+    except TweepError:
+        print("Check the username you provided or your connection and try again")
 
 
 def search_users(query):
     """
     This function receives a query from the user and prints the results of the search
     """
-    results = api.search_users(query)
-    print('\nThe search for {} has returned the following results:'.format(query))
-    for result in results:
-        print('\t'+result.screen_name)
-    print('')
-    return results
+    try:
+        results = api.search_users(query)
+        print('\nThe search for {} has returned the following results:'.format(query))
+        for result in results:
+            print('\t' + result.screen_name)
+        print('')
+        return results
+    except TweepError:
+        print("Check the query you provided or your connection and try again")
 
 
 def get_user_friends(username):
     """
     This function receives a username and returns the usernames of the users the user follows
     """
-    results = api.friends(username)
-    print('\n{} follows:'.format(username))
-    for result in results:
-        print('\t'+result.screen_name)
-    print('')
+    try:
+        results = api.friends(username)
+        print('\n{} follows:'.format(username))
+        for result in results:
+            print('\t' + result.screen_name)
+        print('')
+    except TweepError:
+        print("Check the username you provided or your connection and try again")
 
 
 def get_user_followers(username):
     """
     This function receives a username and returns the usernames of the users that follow the user
     """
-    results = api.followers(username)
-    print('\n{}\'s followers:'.format(username))
-    for result in results:
-        print('\t'+result.screen_name)
-    print('')
-
-
-def main():
-    """
-    This is the main function that receives arguments from the command line and calls the respectful functions
-    """
-    if len(sys.argv) <= 2:
-        print(usage)
-    elif len(sys.argv) > 2:
-        if sys.argv[1] == "--user":
-            if sys.argv[2]:
-                try:
-                    get_user_details(sys.argv[2])
-                except TweepError:
-                    print("Check the username you provided and try again and try again")
-            else:
-                print("Usage: python twitter_on_cli.py --user [username]")
-
-        elif sys.argv[1] == "--search":
-            if sys.argv[2]:
-                try:
-                    search_results = search_users(sys.argv[2])
-                except TweepError:
-                    print("Check the query you provided and try again and try again")
-            else:
-                print("Usage: python twitter_on_cli.py --search [name]")
-
-        elif sys.argv[1] == "--timeline":
-            if sys.argv[2]:
-                try:
-                    get_user_tweets(sys.argv[2])
-                except TweepError:
-                    print("Check the username you have provided and try again")
-            else:
-                print("Usage: python twitter_on_cli.py --timeline [username]")
-
-        elif sys.argv[1] == "--followers":
-            if sys.argv[2]:
-                try:
-                    get_user_followers(sys.argv[2])
-                except TweepError:
-                    print("Check the username you have provided and try again")
-            else:
-                print("Usage: python twitter_on_cli.py --followers [username]")
-
-        elif sys.argv[1] == "--friends":
-            if sys.argv[2]:
-                try:
-                    get_user_friends(sys.argv[2])
-                except TweepError:
-                    print("Check the username you provided and try again")
-            else:
-                print("Usage: python twitter_on_cli.py --friends [username]")
-
-        elif sys.argv[1] == "--help":
-            print(usage)
-
-        else:
-            print(usage)
-
-
-if __name__ == '__main__':
-    main()
+    try:
+        results = api.followers(username)
+        print('\n{}\'s followers:'.format(username))
+        for result in results:
+            print('\t' + result.screen_name)
+        print('')
+    except TweepError:
+        print("Check the username you provided or your connection and try again")
