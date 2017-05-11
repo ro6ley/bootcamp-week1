11393c8a3d9ae19d62bffb49c7e2724d8631d4ce
Robley Gori <robleyadrian@gmail.com>
2017-05-11 14:48:21 +0300

    Added the binary search implementation

--- Day 4/Andelabs/Binary Search/binary_search.py
+++ Day 4/Andelabs/Binary Search/binary_search.py
@@ -11,6 +11,7 @@ class BinarySearch(list):
     """
     def __init__(self, a, b):
         data = []
+        data.append(b)
         list_len = 1
         while list_len < a:
             data.append(data[list_len - 1] + b)
@@ -18,6 +19,45 @@ class BinarySearch(list):
         super(BinarySearch, self).__init__(data)
         self.length = len(data)
 
+    # Binary search implementation
     def search(self, number):
-        pass
+        count = 0
+        first = 0
+        self.length = len(self)
+        last = self.length - 1
+        mid_point = int((last) / 2)
+        obj_location = {'count': '', 'index': ''}
+        while first < mid_point:
+            if (first == mid_point) and (self[first] > number):
+                index = -1
+                obj_location["count"] = self.length
+                obj_location["index"] = index
+                return obj_location
+            elif self[first] == number:
+                index = first
+                obj_location["count"] = count
+                obj_location["index"] = index
+                return obj_location
+            elif self[last] == number:
+                index = last
+                obj_location["count"] = count
+                obj_location["index"] = index
+                return obj_location
+            elif self[mid_point] == number:
+                index = mid_point
+                obj_location["count"] = count
+                obj_location["index"] = index
+                return obj_location
+            else:
+                if self[mid_point] > number:
+                    last = mid_point - 1
+                    first = first + 1
+                    mid_point = (last + first) // 2
+                else:
+                    first = mid_point + 1
+                    last = last - 1
+                    mid_point = ((last + first) // 2) + 1
+            count += 1
+        obj_location = {'count': count, 'index': -1}
+        return obj_location
 
